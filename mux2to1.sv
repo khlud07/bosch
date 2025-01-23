59b57f2a53907540be4ed99878bce13a53dc2a37
khlud88 <166555384+khlud88@users.noreply.github.com>
2025-01-23 15:41:44 +0200

    Update mux2to1.sv

--- mux2to1.sv
+++ mux2to1.sv
@@ -1,7 +1,7 @@
 // Everything is cool, need to fix inputs names --> see adder comment
 module mux2to1 (
-    input logic a, b, sel,
-    output logic y
+    input logic a_i, b_i, sel_i,
+    output logic y_o
 );
-    assign y = (sel) ? b : a;
+    assign y_o = (sel_i) ? b_i : a_i;
 endmodule
