16932a24e6e74d0983b99d6f67ebcab10ed0a460
khlud88 <166555384+khlud88@users.noreply.github.com>
2025-01-23 15:58:11 +0200

    Update mux4to1.sv

--- mux4to1.sv
+++ mux4to1.sv
@@ -1,24 +1,24 @@
 // Everything is cool, need to fix inputs names --> see adder comment
 module mux4to1 (
-    input logic a,      // Input a
-    input logic b,      // Input b
-    input logic c,      // Input c
-    input logic d,      // Input d
-    input logic [1:0] sel, // 2-bit select line
-    output logic y      // Output y
+    input logic a_i,      // Input a
+    input logic b_i,      // Input b
+    input logic c_i,      // Input c
+    input logic d_i,      // Input d
+    input logic [1:0] sel_i, // 2-bit select line
+    output logic y_o      // Output y
 );
 
     always_comb begin
-        case (sel)
+        case (sel_i)
         // You could even write: 
         // 0: y = a;
         // 1: y = b;
         // Just shorter line of code (also depends on your task, if you want to highlight bit behaviour it still looks ok)
-            2'b00: y = a; // Select input a
-            2'b01: y = b; // Select input b
-            2'b10: y = c; // Select input c
-            2'b11: y = d; // Select input d
-            default: y = 1'b0; // Default case (not strictly needed for 2-bit sel)
+            0: y_o = a_i; // Select input a
+            1: y_o = b_i; // Select input b
+            2: y_o = c_i; // Select input c
+            3: y_o = d_i; // Select input d
+            default: y_o = 1'b0; // Default case (not strictly needed for 2-bit sel)
         endcase
     end
 
